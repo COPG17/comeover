11f057a0e03135cef709e067a980c30a7f485626
Zayne Melnick <96027727+melnickz@users.noreply.github.com>
2022-11-11 15:05:12 -0500

    Added back4app info to appdelegate

--- comeover/comeover/AppDelegate.swift
+++ comeover/comeover/AppDelegate.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
-
+import Parse
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = "Iyjvpez40Nf78wwr9dxGYW8NCEHNBIUu7ni8Gu1D"
+            $0.clientKey = "jbruwOm7ICKGteibQHUfJsqa50ieXnt29sJR6YeY"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
         return true
     }
 
